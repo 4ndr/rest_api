bdafa965d86b0b517a9a47702a080234c56e5083
4ndr <andrepereiraleite@gmail.com>
2018-02-04 19:29:07 -0300

    Created our first api view

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('profiles_api.urls'))
 ]
