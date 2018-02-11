ec803b711cd042f00ea6e86681fa3ed9d0ef016d
4ndr <andrepereiraleite@gmail.com>
2018-02-11 10:22:09 -0300

    Added model for stoge ProfileFieldItem and runned migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -64,3 +64,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 		return self.email
 
 		
+class ProfileFeedItem(models.Model):
+	"""Profile status update"""
+	
+	user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+	status_text = models.CharField(max_length=255)
+	created_on = models.DateTimeField(auto_now_add=True)
+
+
+	def __str__(self):
+		"""return a modern as string"""
+
+		return self.status_text
+		
